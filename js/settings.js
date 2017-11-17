6836b0089c6c13bb196eba46531a0c0ca64b0b11
Kishor Narsingrao <kishor5.garje@gmail.com>
2017-11-17 17:40:20 +0530

    Update settings.js
    Updated as per Malay language

--- js/settings.js
+++ js/settings.js
@@ -79,6 +79,7 @@ var languageSettings = {
     "defaultLanguage": "en",
     "languages": {
         "en": "English (EN)",
+        "my": "Malay (MY)",
         "es": "Espa&ntilde;ol (ES)"
     }
 };
